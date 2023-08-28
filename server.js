08894a941cd4a429f77188e8d91dd0cb9a9706e3
Nikhil Pujar <nikhilpujar23@gmail.com>
2023-08-28 10:53:44 +0530

    final

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
+const path = require('path');
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
 
-const app = express();
+
+
 const port = 3000;
 
-// Configure Multer for file upload
+
 const storage = multer.diskStorage({
+
+
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+        cb(null, 'uploads/');
   },
+
+
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+        cb(null, file.fieldname  + path.extname(file.originalname));
   }
 });
 
 const upload = multer({ storage });
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
 
-app.post('/upload', upload.single('file'), (req, res) => {
+ express().use(express.static('public'));
+
+ express().post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
     const uploadedFile = {
-      originalname: req.file.originalname,
-      mimetype: req.file.mimetype,
-      size: req.file.size,
-      destination: req.file.destination,
-      filename: req.file.filename,
+          mimetype: req.file.mimetype,
+          originalname: req.file.originalname,
+          destination: req.file.destination,
+          filename: req.file.filename,
+          size: req.file.size,
     };
-    res.json({ message: 'File uploaded successfully.', file: uploadedFile });
+    res.json({ message: 'File uploaded.', file: uploadedFile });
   } else {
-    res.status(400).send('No file uploaded.');
+    res.status(400).send('File not Uploaded.');
   }
 });
 
-app.listen(port, () => {
+ express().listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
